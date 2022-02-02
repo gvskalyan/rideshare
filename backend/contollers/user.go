3d97c6e6f74692596f73f8c8f05df4acf4c88c1a
kalyanGvs <gvskalyan2@gmail.com>
2022-02-01 21:20:20 -0500

    add CRUD functionality for user

---
+++ backend/contollers/user.go
@@ -0,0 +1,158 @@
+package controllers
+
+import (
+	"auth/models"
+	"auth/utils"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/gorilla/mux"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type ErrorResponse struct {
+	Err string
+}
+
+type error interface {
+	Error() string
+}
+
+var db = utils.ConnectDB()
+
+func MagaAPI(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("i am here")
+}
+
+func TestAPI(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("API live and kicking"))
+}
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	user := &models.User{}
+	err := json.NewDecoder(r.Body).Decode(user)
+	if err != nil {
+		var resp = map[string]interface{}{"status": false, "message": "Invalid request"}
+		json.NewEncoder(w).Encode(resp)
+		return
+	}
+	resp := FindOne(user.Email, user.Password)
+	json.NewEncoder(w).Encode(resp)
+}
+
+func FindOne(email, password string) map[string]interface{} {
+	user := &models.User{}
+
+	if err := db.Where("Email = ?", email).First(user).Error; err != nil {
+		var resp = map[string]interface{}{"status": false, "message": "Email address not found"}
+		return resp
+	}
+	expiresAt := time.Now().Add(time.Minute * 100000).Unix()
+
+	errf := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+	if errf != nil && errf == bcrypt.ErrMismatchedHashAndPassword { //Password does not match!
+		var resp = map[string]interface{}{"status": false, "message": "Invalid login credentials. Please try again"}
+		return resp
+	}
+
+	tk := &models.Token{
+		UserID: user.ID,
+		Name:   user.Name,
+		Email:  user.Email,
+		StandardClaims: &jwt.StandardClaims{
+			ExpiresAt: expiresAt,
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.GetSigningMethod("HS256"), tk)
+
+	tokenString, error := token.SignedString([]byte("secret"))
+	if error != nil {
+		fmt.Println(error)
+	}
+
+	var resp = map[string]interface{}{"status": false, "message": "logged in"}
+	resp["token"] = tokenString //Store the token in the response
+	resp["user"] = user
+	return resp
+}
+
+//CreateUser function -- create a new user
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+
+	user := &models.User{}
+
+	json.NewDecoder(r.Body).Decode(user)
+	gender := user.Gender
+	location := user.City
+	pass, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		fmt.Println(err)
+		err := ErrorResponse{
+			Err: "Password Encryption  failed",
+		}
+		json.NewEncoder(w).Encode(err)
+	}
+
+	user.Password = string(pass)
+	user.Gender = string(gender)
+	user.City = string(location)
+
+	createdUser := db.Create(user)
+	var errMessage = createdUser.Error
+
+	if createdUser.Error != nil {
+		fmt.Println(errMessage)
+	}
+	json.NewEncoder(w).Encode(createdUser)
+}
+
+// type User struct {
+// 	gorm.Model
+
+// 	Name        string
+// 	Password    string `json:"Password"`
+// 	Email       string `gorm:"type:varchar(100);unique_index"`
+// 	DOB         string `json:"DOB"`
+// 	Gender      string `json:"Gender"`
+// 	PhoneNumber string `json:"PhoneNumber"`
+// 	City        string `json:"City"`
+// }
+
+//FetchUser function
+func FetchUsers(w http.ResponseWriter, r *http.Request) {
+	var users []models.User
+	db.Preload("auths").Find(&users)
+
+	json.NewEncoder(w).Encode(users)
+}
+
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+	user := &models.User{}
+	params := mux.Vars(r)
+	var id = params["id"]
+	db.First(&user, id)
+	json.NewDecoder(r.Body).Decode(user)
+	db.Save(&user)
+	json.NewEncoder(w).Encode(&user)
+}
+
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var id = params["id"]
+	var user models.User
+	db.First(&user, id)
+	db.Delete(&user)
+	json.NewEncoder(w).Encode("User deleted")
+}
+
+func GetUser(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var id = params["id"]
+	var user models.User
+	db.First(&user, id)
+	json.NewEncoder(w).Encode(&user)
+}
