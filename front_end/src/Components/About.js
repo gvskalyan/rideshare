12fab8b5dad00fcc3c788f5e934c02dcd205f102
pkvarma222 <pkvarma222@gmail.com>
2022-02-04 17:45:24 -0500

    Updated About page

--- front_end/src/Components/About.js
+++ front_end/src/Components/About.js
@@ -4,7 +4,8 @@ import "./Home.css";
 function About(){
     return (
         <div>
-            <h1> About </h1>
+            <h1> About Us </h1>
+            <h2>RideShare makes travel more convinient and affordable to the users. If you are travelling with a few empty seats, we will try to make your trip a bit profitable by finding you some good company.</h2>
         </div>
     );
 }
