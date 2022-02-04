c1aae66949cf15cfa0823159fa0d371bcd94ace7
Ruiyang Li <ruiyangl@outlook.com>
2022-02-04 17:56:19 -0500

    fix corrupted app.js
    fix corrupted app.js

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,28 +1,3 @@
-<<<<<<< Updated upstream
-import React from "react";
-import Find from "./Components/Find";
-import Post from "./Components/Post";
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
-import "./App.css";
-import NavBar from "./Components/NavBar"
-import Global from "./Components/Home"
-
-function App() {
-  return (
-    <>
-    <Router>
-        <NavBar />
-        <div className="extra"></div>
-      <div className="App">
-      <Switch>
-        <Route exact path="/" component={Global}/>
-        <Route exact path="/Find" component={Find}/>
-        <Route exact path="/Post" component={Post} />
-      </Switch>
-      </div>
-    </Router>     
-    </>
-=======
 import logo from './logo.svg';
 import './App.css';
 
@@ -44,7 +19,6 @@ function App() {
         </a>
       </header>
     </div>
->>>>>>> Stashed changes
   );
 }
 
