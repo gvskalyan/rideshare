9eb4029731e6d64a3c8f298d2722d621dc91c518
Ruiyang Li <ruiyangl@outlook.com>
2022-02-04 17:45:37 -0500

    Fixing bugs on initial project creation
    Fixing bugs on initial project creation

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from "react";
 import Find from "./Components/Find";
 import Post from "./Components/Post";
@@ -21,6 +22,29 @@ function App() {
       </div>
     </Router>     
     </>
+=======
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+>>>>>>> Stashed changes
   );
 }
 
--- front_end/src/index.js
+++ front_end/src/index.js
@@ -11,4 +11,10 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+<<<<<<< Updated upstream
+=======
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+>>>>>>> Stashed changes
 reportWebVitals();
