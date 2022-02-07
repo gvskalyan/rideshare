96d31e5a7fb7ba854b38d80db14db59dc38e8584
kalyanGvs <gvskalyan2@gmail.com>
2022-02-07 10:02:51 -0500

    change front_end folder name to frontend

---
+++ frontend/src/Components/Home.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState} from 'react'
+import "./Home.css";
+
+function Home(){
+    return (
+        <div>
+            <h1> About </h1>
+        </div>
+    );
+}
+
+export default Home;
