f80a518fb2a731e90855e1e55e5facbcffb2db35
pkvarma222 <pkvarma222@gmail.com>
2022-03-02 15:17:29 -0500

    Updated Home/About page

--- front_end/src/Components/About.js
+++ front_end/src/Components/About.js
@@ -1,13 +1,64 @@
 import React, { useEffect, useState} from 'react'
-import "./Home.css";
-
+import gettingin from "./assets/gettingin.jpg"
+import styled from "styled-components"
 function About(){
     return (
-        <div>
-            <h1> About Us </h1>
-            <h2>RideShare makes travel more convinient and affordable to the users. If you are travelling with a few empty seats, we will try to make your trip a bit profitable by finding you some good company.</h2>
+        <Section id="hero">
+      <div className="background">
+        <img src={gettingin} alt="" />
+      </div>
+      <div className="content">
+        <div className="title">
+          <h1>ABOUT US</h1>
+          <p>
+          RideShare makes travel more convinient and affordable to the users. 
+          If you are travelling with a few empty seats, we will try to make your trip a bit profitable
+          by finding you some good company.
+          </p>
         </div>
-    );
+      </div>
+    </Section>
+  );
 }
+
+const Section = styled.section`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  .background {
+    img {
+      height: 15%;
+      width: 100%;
+      filter: brightness(60%);
+    }
+  }
+  .content {
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    top: 0;
+    z-index: 3;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+    .title {
+      color: white;
+      h1 {
+        margin-top: 0rem;
+        font-size: 3rem;
+        letter-spacing: 0.2rem;
+      }
+      p {
+        text-align: center;
+        padding: 0 15vw;
+        margin-top: 0.5rem;
+        font-size: 1.2rem;
+      }
+    }
+  }
+`;
 
 export default About;
