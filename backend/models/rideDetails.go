c6815880e2ed3a7157c97ac65b885aaf5d35e872
DheeruRao <dheerajrao003@gmail.com>
2022-03-15 14:28:57 -0400

    refactor and add search a ride functionality

---
+++ backend/models/rideDetails.go
@@ -0,0 +1,28 @@
+package models
+
+import (
+	"time"
+
+	"github.com/jinzhu/gorm"
+)
+
+//User struct declaration
+type RideDetails struct {
+	gorm.Model
+	Name           string `json:"Name"`
+	FromCity       string `json:"fromCity"`
+	StartLocation  string `json:"startLocation"`
+	ToCity         string `json:"toCity"`
+	DestLocation   string `json:"destLocation"`
+	Price          uint   `json:"priceperseat"`
+	RideDuration   uint   `json:"Duration"`
+	SeatsAvailable uint   `json:"seatsAvailable"`
+	PetsAllowed    string `json:"petsAllowed"`
+	//CarModel       string `json:"carModel"`
+	//CarType        string `json:"carType"`
+	AddlNotes   string `json:"addlNotes"`
+	PhoneNumber string `json:"PhoneNumber"`
+	UserId      int
+	ToStartTime time.Time
+	ToEndTime   time.Time
+}
