8979faa78ca679c410c722e3e97a5cce5209058e
kalyanGvs <gvskalyan2@gmail.com>
2022-03-23 15:12:18 -0400

    refactor mail function

--- backend/controllers/emailconfirmation.go
+++ backend/controllers/emailconfirmation.go
@@ -17,6 +17,8 @@ func ConfirmationEmailHandler(mailid string) {
 	fromMail = os.Getenv("FromEMailAddress")
 	msg.SetHeader("From", fromMail)
 	msg.SetHeader("To", mailid)
+	msg.SetHeader("Subject", "RideShare Booked Ride details")
+	msg.SetBody("text/html", "Greetings from Rideshare team <br /> Your ride ID is: "+" and the ride details are <br /> Hope you have a great and pleasant ride. <br />")
 
 	n := gomail.NewDialer("smtp.mail.yahoo.com", 587, fromMail, os.Getenv("SMTPPassword"))
 	if err := n.DialAndSend(msg); err != nil {
