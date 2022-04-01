8a2d31fb878df50942d457953cd53e462827b4d0
DheeruRao <dheerajrao003@gmail.com>
2022-03-31 20:31:45 -0400

    edit routes

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -2,8 +2,6 @@ package routes
 
 import (
 	"backend/controllers"
-	"backend/utils/auth"
-	"net/http"
 
 	"github.com/gorilla/mux"
 )
@@ -17,24 +15,16 @@ func Handlers() *mux.Router {
 	// r.HandleFunc("/api", controllers.TestAPI).Methods("GET")
 	r.HandleFunc("/signup", controllers.CreateUser).Methods("POST")
 	r.HandleFunc("/login", controllers.Login).Methods("POST")
+	r.HandleFunc("/deleteUser", controllers.DeleteUser).Methods("DELETE")
+	r.HandleFunc("/logout", controllers.Logout).Methods("POST")
+	r.HandleFunc("/postaride", controllers.PostARide).Methods("POST")
+	r.HandleFunc("/searcharide", controllers.SearchARide).Methods("POST")
+
+	//r.HandleFunc("/bookride", controllers.BookRide).Methods("POST")
 
 	// Auth route
 	s := r.PathPrefix("/auth").Subrouter()
-	s.Use(auth.JwtVerify)
-	s.HandleFunc("/user", controllers.FetchUsers).Methods("GET")
-	s.HandleFunc("/user/{id}", controllers.GetUser).Methods("GET")
-	s.HandleFunc("/user/{id}", controllers.UpdateUser).Methods("PUT")
-	s.HandleFunc("/user/{id}", controllers.DeleteUser).Methods("DELETE")
+	s.HandleFunc("/user", controllers.GetUser).Methods("GET")
+	s.HandleFunc("/user", controllers.UpdateUser).Methods("PUT")
 	return r
-}
-
-// CommonMiddleware --Set content-type
-func CommonMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Content-Type", "application/json")
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Access-Control-Request-Headers, Access-Control-Request-Method, Connection, Host, Origin, User-Agent, Referer, Cache-Control, X-header")
-		next.ServeHTTP(w, r)
-	})
 }
