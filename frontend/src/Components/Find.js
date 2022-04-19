1c624f72a2e5035b9810175e9098c94c62b49fce
Riyaz Shaik <riyaz.kvgb@gmail.com>
2022-04-19 15:15:38 -0400

    ride booking functionality integration

--- frontend/src/Components/Find.js
+++ frontend/src/Components/Find.js
@@ -97,35 +97,13 @@ class MyComponent extends Component {
             "UserMail": "${user.Email}",
             "Phone": "${user.PhoneNumber}"
         }`;
-        alert("Ride booked");
+
+        data.bookaride(booking).then(res => {
+            alert("Ride booked successfully");
+        });
     }
 
     render() {
-
-        const card = (
-            <React.Fragment>
-                <CardContent>
-                    <Typography sx={{fontSize: 14}} color="text.secondary" gutterBottom>
-                        Word of the Day
-                    </Typography>
-                    <Typography variant="h5" component="div">
-                        benevolent
-                    </Typography>
-                    <Typography sx={{mb: 1.5}} color="text.secondary">
-                        adjective
-                    </Typography>
-                    <Typography variant="body2">
-                        well meaning and kindly.
-                        <br/>
-                        {'"a benevolent smile"'}
-                    </Typography>
-                </CardContent>
-                <CardActions>
-                    <Button size="small">Learn More</Button>
-                </CardActions>
-            </React.Fragment>
-        );
-
         return (
             <div>
                 <Section>
